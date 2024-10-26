ec1a46e478a9d9002fb5e001dea7eff1077fce08
RafaelRodrigues44 <rafael.rodrigues85@hotmail.com>
2024-10-26 13:30:06 -0300

    criados mais 2 padroes, mediator e singleton

--- characters/models/game.py
+++ characters/models/game.py
@@ -1,13 +1,12 @@
-
 from characters.interfaces.i_game import GameInterface
 from characters.models.player import Player
 
-
 class Game(GameInterface):
-    def __init__(self, players: list[Player]):
+    def __init__(self, players: list[Player], mediator):
         self.players = players
         self.winner = None
-        self.current_turn = 0  # Index of the current player
+        self.current_turn = 0  # Índice do jogador atual
+        self.mediator = mediator  # O mediador é passado na inicialização
 
     def start_game(self):
         print("Iniciando o jogo...")
@@ -25,3 +24,6 @@ class Game(GameInterface):
         # Altera o turno para o próximo jogador
         self.current_turn = (self.current_turn + 1) % len(self.players)
         print(f"Turno mudado para {self.players[self.current_turn].name}")
+        # Notificar o mediador sobre a mudança de turno, se necessário
+        if self.mediator:
+            self.mediator.notify(self.players[self.current_turn], "turn_started")
--- designs_patterns/factory/factories/round_factory.py
+++ designs_patterns/factory/factories/round_factory.py
@@ -3,7 +3,8 @@ from typing import List
 from characters.models.player import Player
 from characters.models.round import Round
 from designs_patterns.factory.interfaces.i_factory import IFactory
+from designs_patterns.mediator.game_mediator import GameMediator
 
 class RoundFactory(IFactory):
-    def create(self, players: List[Player]) -> Round:
-        return Round(players)
+    def create(self, players: List[Player], mediator: GameMediator) -> Round:
+        return Round(players, mediator)
